329271b691f572c3d4d4870b559e128c1db8b765
meihuang <meihuang8328@gmail.com>
2022-11-21 17:20:48 -0500

    my_flatten

--- Enumerables/Array.rb
+++ Enumerables/Array.rb
@@ -3,22 +3,22 @@ class Array
     def my_flatten
         array = []
 
+        return [] if self.empty?
+
         self.each do |ele|
-            if ele == ele.kind_of?(Array)
-                array.push(*ele)
+            if ele.kind_of?(Array)
+                array.concat(ele.my_flatten)
             else
                 array << ele
             end
         end
 
-        if self.all? { |ele| ele != ele.kind_of?(Array) }
-            return array
-        else
-            self[1..-1].my_flatten
-        end
+       array
 
     end
 
     p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+    
 
 end
