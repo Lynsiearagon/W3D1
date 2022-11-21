ebee2d0775374b3c662bd84dfb34395803ec02c4
meihuang <meihuang8328@gmail.com>
2022-11-21 16:16:47 -0500

    Enumerables part

--- Enumerables/Enumerable.rb
+++ Enumerables/Enumerable.rb
@@ -1,11 +1,12 @@
+require 'byebug'
+
 class Array
 
     def my_each(&blk)
         i = 0
 
         while i < self.length
-            new_i = blk.call(self[i])
-            self[i] = new_i
+            blk.call(self[i])
             i += 1
         end
 
@@ -24,11 +25,44 @@ class Array
         arr
     end
 
+    # a = [1, 2, 3]
+    # p a.my_select { |num| num > 1 } # => [2, 3]
+    # p a.my_select { |num| num == 4 } # => []
+    
+    def my_reject(&blk)
+        rejected = []
+
+
+        self.my_each do |ele|
+            rejected << ele if !blk.call(ele)
+    
+        end
+
+
+        rejected
+    end
+    
+    # a = [1, 2, 3]
+    # p a.my_reject { |num| num > 1 } # => [1]
+    # p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+    def my_any?(&blk)
+        self.my_each { |ele| return true if blk.call(ele) }
+        false
+    end
+
     a = [1, 2, 3]
-    p a.my_select { |num| num > 1 } # => [2, 3]
-    p a.my_select { |num| num == 4 } # => []
-    
+    # p a.my_any? { |num| num > 1 } # => true
+    # p a.my_any? { |num| num == 4 } # => false
+
+    def my_all?(&blk)
+        self.my_each { |ele| return false if !blk.call(ele) }
+        true
+    end
+
+
+    p a.my_all? { |num| num > 1 } # => false
+    p a.my_all? { |num| num < 4 } # => true
 
     
-
 end
